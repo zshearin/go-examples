c07b0745d1d48d96c3845f640c30f8245b7dde3d
zshearin <zshearin617@gmail.com>
2021-10-13 16:11:02 -0400

    add encryption example

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	//"github.com/zshearin/go-examples/basic"
-	ifc "github.com/zshearin/go-examples/interface-cast"
+	//ifc "github.com/zshearin/go-examples/interface-cast"
+	"github.com/zshearin/go-examples/encryption"
 )
 
 func main() {
 
 	//sort.DoExample()
-
 	//context.DoExample()
-
 	//basic.DoExample()
-	
-	ifc.DoExample()
-
+	//ifc.DoExample()
 	//string.DoExample()
+	encryption.DoExample()
 }
